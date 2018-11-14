c07e2cda16f03e463561f377418fcd7350220126
Gisia <Gisia@users.noreply.github.com>
2018-11-14 18:38:09 -0400

    Programming Assignment 2
    This is the program 'cachematrix.R' created as an assignment for the course 'R Programming' in Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program has two main functions. The object of the program is to check if the inverse
+## of a matrix is already calculated. In that case, the program retrieves the cached value. 
+## Otherwise, it calculates the inverse of the matrix and stores it in the object 'sol'. 
 
-## Write a short comment describing this function
+## This function takes a matrix, x, and returns a list with the matrix and its inverse
+## along with a complete copy of the environment in which the function 'makeCacheMatrix'
+## was written. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    sol <- NULL
+    set <- function(y) {
+      x <<- y
+      sol <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) sol<<- solve
+    getsolve <- function() sol
+    list(set=set, get = get, 
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
+  
+## This function takes a matrix in the form provided by 'makeCacheMatrix', checks whether
+## its inverse exists and if it does, gets if from the cache. Otherwise it solves the matrix
+## for its inverse and stores the result in the object 'sol'. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  sol <- x$getsolve()
+  if(!is.null(sol)) {
+    message("getting cached data")
+    return(sol)
+  }
+  data <- x$get()
+  sol <- solve(data,...)
+  x$setsolve(sol)
+  sol
 }
